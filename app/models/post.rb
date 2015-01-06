ba3f00d1362923c14d7f3cab984d53271932f0bf
vagrant <rachelb@gmail.com>
2015-01-06 19:32:19 +0000

    Added validation for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,9 @@ class Post < ActiveRecord::Base
   belongs_to :job
 
   default_scope { order('created_at DESC') }
+
+  validates :title, length: { minimum: 4 }, presence: true
+  validates :body, length: { minimum: 10 }, presence: true
+  validates :job, presence: true
+  validates :user, presence: true
 end
