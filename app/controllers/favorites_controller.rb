f104e482bbf88993162e8b0209446782bef4e6ed
vagrant <rachelb@gmail.com>
2015-02-11 19:41:43 +0000

    Added destroy to Favorites Controller and styled favorite button

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -13,6 +13,19 @@ class FavoritesController < ApplicationController
     end
   end
 
+  def destroy
+    @favorite = Favorite.find(params[:id])
+    authorize @favorite
+     
+    if @favorite.destroy
+      flash[:notice] = "Unfavorited."
+      redirect_to :back
+    else
+      flash[:error] = "Failed to unfavorite."
+      redirect_to :back
+    end    
+  end
+
   private
 
   def favorite_params
