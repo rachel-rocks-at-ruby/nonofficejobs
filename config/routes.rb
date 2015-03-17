b44ab3e967513e3ef0b7338e3b321c73896768c1
vagrant <rachelb@gmail.com>
2015-03-17 01:37:41 +0000

    Changed Omniauth route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   resources :users, only: [:update, :index, :show]
 
   get 'about' => 'welcome#about'
